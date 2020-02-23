426721ee493a48ee003078c88c7011df66877153
Kevin Chen <kevchen2@gmail.com>
2020-02-23 10:03:46 -0800

    Initial setup of advance example

---
+++ Examples/AdvanceExample/AdvanceExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AdvanceExample
+//
+//  Created by Kevin Chen on 2/23/20.
+//  Copyright © 2020 Kevin Chen. All rights reserved.
+//
+
+import UIKit
+
+// A more advance example of the drawer with a navigation controller, table view, animator that changes with drawer movement, and touch events for the grabber view. Each vc in the nav. cont. has different drawer steps.
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
